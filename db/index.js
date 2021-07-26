38cf2b215ea29e2fc4b4b96e1db3ad0552da70dc
Charlee <CharleeBrown22@gmail.com>
2021-07-26 09:59:29 -0500

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 var http = require('http');
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://lee:Gamez2232!@c0ne.5lyko.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 http.createServer(function (req, res) {
     fs.readFile(__dirname + req.url, function (err,data) {
@@ -13,11 +11,7 @@ http.createServer(function (req, res) {
       }
       res.writeHead(200);
       res.end(data);
-  
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+
 });
 });
 }).listen(8080);
