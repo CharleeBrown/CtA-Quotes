116912edbd9475443f0060cea5700a227f55039a
CharlesLee-23 <Coleman.hallom@Spray.com>
2021-07-26 10:21:08 -0500

    Clearing whitespace.

--- db/index.js
+++ db/index.js
@@ -4,10 +4,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     fs.readFile(__dirname + req.url, function (err,data) {
-      
       res.writeHead(200);
-      res.write("Hello");
       res.end(data);
-
 });
 }).listen(8080);
