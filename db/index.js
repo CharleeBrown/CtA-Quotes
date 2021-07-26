fd57e601ab28f3eb1bb191a784faaf98275b6027
CharlesLee-23 <Coleman.hallom@Spray.com>
2021-07-26 10:13:30 -0500

    Moved everything to db folder to make server work.

--- db/index.js
+++ db/index.js
@@ -4,14 +4,10 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     fs.readFile(__dirname + req.url, function (err,data) {
-      if (err) {
-        res.writeHead(404);
-        res.end(JSON.stringify(err));
-        return;
-      }
+      
       res.writeHead(200);
+      res.write("Hello");
       res.end(data);
 
-});
 });
 }).listen(8080);
