0618529b7e29fb467550a0e5d4dff331ed259ce0
CharlesLee-23 <Coleman.hallom@Spray.com>
2021-07-28 09:57:00 -0500

    Added more placeholder quotes.

--- db/script.js
+++ db/script.js
@@ -8,8 +8,23 @@ function createQuote(){
     let qt1 = "When you think of garbage, think of Hakeem";
     let qt2 = "You rhinocerous pizzel"; 
     let qt3 = "They have the golden arches, we have the golden arcs";
+    let qt3 = "They have the golden arches, we have the golden arcs ";
+    let qt3 = "They have the golden arches, we have the golden arcs";
+    let qt3 = "They have the golden arches, we have the golden arcs";
+    let qt3 = "They have the golden arches, we have the golden arcs";
+    let qt3 = "They have the golden arches, we have the golden arcs";
+    let qt3 = "They have the golden arches, we have the golden arcs";
+    let qt3 = "They have the golden arches, we have the golden arcs";
+    let qt3 = "They have the golden arches, we have the golden arcs";
+    let qt3 = "They have the golden arches, we have the golden arcs";
+    let qt3 = "They have the golden arches, we have the golden arcs";
+    let qt3 = "They have the golden arches, we have the golden arcs";
+    let qt3 = "They have the golden arches, we have the golden arcs";
+
     let quotes = [qt1, qt2, qt3];
-    quote.innerText = quotes[getRandom(quotes.length)];
+    let randomNum = getRandom(quotes.length);
+    console.log(randomNum);
+    quote.innerText = quotes[randomNum];
     //console.log(quotes[getRandom(quotes.length)]);
    // console.log(quotes[rando.].toString())
  //   let speech = "Coleman charles Lee";
